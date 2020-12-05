757f4c672577d27a6f330697c7c152c53d77d2e6
ali <ali@g.com>
2020-12-05 15:48:54 +0100

    Todo App 2.0 finish

---
+++ TodoApp/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace TodoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvTodo = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textTodo = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Task = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Done = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Deadline = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTodo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvTodo
+            // 
+            this.dgvTodo.AllowUserToDeleteRows = false;
+            this.dgvTodo.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgvTodo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTodo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.Task,
+            this.Done,
+            this.Deadline});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.LimeGreen;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvTodo.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvTodo.Location = new System.Drawing.Point(420, 31);
+            this.dgvTodo.Name = "dgvTodo";
+            this.dgvTodo.ReadOnly = true;
+            this.dgvTodo.RowHeadersWidth = 51;
+            this.dgvTodo.RowTemplate.Height = 24;
+            this.dgvTodo.Size = new System.Drawing.Size(614, 380);
+            this.dgvTodo.TabIndex = 0;
+            this.dgvTodo.DoubleClick += new System.EventHandler(this.dgvTodo_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Todo Hinzufügen";
+            // 
+            // textTodo
+            // 
+            this.textTodo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textTodo.Location = new System.Drawing.Point(21, 180);
+            this.textTodo.Name = "textTodo";
+            this.textTodo.Size = new System.Drawing.Size(248, 30);
+            this.textTodo.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(21, 31);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(378, 30);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(63, 372);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 39);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Add";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(173, 372);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(96, 39);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(292, 372);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(87, 39);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "id";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Visible = false;
+            this.id.Width = 125;
+            // 
+            // Task
+            // 
+            this.Task.DataPropertyName = "Task";
+            this.Task.HeaderText = "Task";
+            this.Task.MinimumWidth = 6;
+            this.Task.Name = "Task";
+            this.Task.ReadOnly = true;
+            this.Task.Width = 125;
+            // 
+            // Done
+            // 
+            this.Done.DataPropertyName = "Done";
+            this.Done.HeaderText = "Erledigt";
+            this.Done.MinimumWidth = 6;
+            this.Done.Name = "Done";
+            this.Done.ReadOnly = true;
+            this.Done.Width = 125;
+            // 
+            // Deadline
+            // 
+            this.Deadline.DataPropertyName = "Deadline";
+            this.Deadline.HeaderText = "Frist Datum";
+            this.Deadline.MinimumWidth = 6;
+            this.Deadline.Name = "Deadline";
+            this.Deadline.ReadOnly = true;
+            this.Deadline.Width = 125;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(21, 259);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 21);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Erledigt";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1085, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textTodo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvTodo);
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.Text = "TodoApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTodo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvTodo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textTodo;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Task;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Done;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Deadline;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ TodoApp/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TodoApp
+{
+    public partial class Form1 : Form
+    {
+        TaskTable model = new TaskTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        void Clear()
+        {
+            textTodo.Text = "";
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+            model.id = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+            FillTheDataGridView();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            model.Deadline = dateTimePicker1.Value;
+            model.Task = textTodo.Text.Trim();
+            model.Done = checkBox1.Checked;
+
+            using (TodoEntities db = new TodoEntities())
+            {
+                if (model.id == 0)
+                    db.TaskTable.Add(model);
+                else
+                    db.Entry(model).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            Clear();
+            FillTheDataGridView();
+            MessageBox.Show("Hinzugefügt");
+            
+        }
+
+        void FillTheDataGridView()
+        {
+            using(TodoEntities db = new TodoEntities())
+            {
+                dgvTodo.DataSource = db.TaskTable.ToList<TaskTable>();
+            }
+        }
+
+        private void dgvTodo_DoubleClick(object sender, EventArgs e)
+        {
+            if(dgvTodo.CurrentRow.Index != -1)
+            {
+                model.id = Convert.ToInt32(dgvTodo.CurrentRow.Cells["id"].Value);
+                using (TodoEntities db = new TodoEntities())
+                {
+                    model = db.TaskTable.Where(x => x.id == model.id).FirstOrDefault();
+                    textTodo.Text = model.Task;
+                    dateTimePicker1.Value = model.Deadline.Date;
+                    checkBox1.Checked = model.Done;
+                        
+                }
+                btnSave.Text = "Update";
+                btnDelete.Enabled = true;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Weiter mit löschen", "EF CRUD Operation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            using(TodoEntities db = new TodoEntities ())
+            {
+                    var entry = db.Entry(model);
+                    if (entry.State == EntityState.Detached)
+                        db.TaskTable.Attach(model);
+                    db.TaskTable.Remove(model);
+                    db.SaveChanges();
+                    FillTheDataGridView();
+                    Clear();
+            }
+        }
+    }
+}
